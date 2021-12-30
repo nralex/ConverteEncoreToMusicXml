a1ec512778e3aca9232723084638a7031d75d963
nralex <alexserno@gmail.com>
2021-12-30 15:15:58 -0300

    Versão 1

--- EncoreToMusicXml.py
+++ EncoreToMusicXml.py
@@ -1,6 +1,33 @@
 import pyautogui as pag
+from time import sleep
+from os import listdir as ld
 
-# Abrir uma lista de arquivos .enc;
-# Abrir cada um dos arquivos, em sequencia;
-# Converter cada arquivo par MusicXml, em uma pasta especificada;
-# Fechar o arquivo e passar para o próximo.
+'''
+Este script é  usado para converter arquivos Encore para arquivos MusicXML.'''
+pag.press('win') # leva até o botão Windows
+sleep(0.5)
+pag.typewrite('encore') # escreve Encore
+sleep(0.5)
+pag.press('enter') # entra no Encore
+arquivos = ld () # lista os arquivos
+for a in arquivos: # para cada arquivo
+    if '.enc' in a: # se for um arquivo Encore
+        sleep(1)
+        pag.hotkey('ctrl', 'o') # abre um arquivo Encore
+        '''
+        Necessita indicar o caminho da pasta onde estão os arquivos Encore.
+        '''
+        sleep(1)
+        pag.write(a) # escreve o nome do arquivo Encore
+        pag.hotkey('alt', 'a')
+        sleep(0.5)
+        pag.keyDown('alt') # converte o arquivo Encore para MusicXML
+        pag.press('f')
+        pag.keyUp('alt')
+        pag.press('down', presses=7)
+        pag.press('right')
+        pag.press('down')
+        pag.press('enter')
+        sleep(0.5)
+        pag.press('enter')
+pag.hotkey('alt', 'f4') # fecha o Encore
